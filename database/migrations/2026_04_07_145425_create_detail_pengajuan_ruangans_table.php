f3603a98b1669bbd7dec5ef8a08ab5064a0ec1a1
spirinity <arkamahardika35@gmail.com>
2026-04-07 23:12:53 +0800

    chore: setup database migrations and spatie layer

---
+++ database/migrations/2026_04_07_145425_create_detail_pengajuan_ruangans_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('detail_pengajuan_ruangans', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('pengajuan_id')->constrained('pengajuans')->cascadeOnDelete();
+            $table->foreignId('ruangan_id')->constrained('ruangans')->cascadeOnDelete();
+            $table->date('tanggal_pakai');
+            $table->time('waktu_mulai');
+            $table->time('waktu_selesai');
+            $table->integer('jumlah_pengguna');
+            $table->text('nama_pengguna_lainnya')->nullable();
+            $table->text('catatan_alat_bahan')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('detail_pengajuan_ruangans');
+    }
+};
---
+++ database/migrations/2026_04_07_145426_create_detail_pengajuan_alats_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('detail_pengajuan_alats', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('pengajuan_id')->constrained('pengajuans')->cascadeOnDelete();
+            $table->foreignId('alat_id')->constrained('alats')->cascadeOnDelete();
+            $table->integer('jumlah_dipinjam');
+            $table->date('tanggal_pinjam');
+            $table->text('keperluan_spesifik');
+            $table->integer('durasi_jam')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('detail_pengajuan_alats');
+    }
+};
